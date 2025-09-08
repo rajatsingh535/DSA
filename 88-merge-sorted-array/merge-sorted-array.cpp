5d9e911b1d1b09f437d0ea6c3cd59c6ba10f5c54
RAJATSINGH <rajat.singh20242@lpu.in>
2025-09-08 19:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.69%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-            int i = m - 1;     // nums1 ka last valid element
-            int j = n - 1;     // nums2 ka last element
-            int k = m + n - 1; // nums1 ka last index (jaha fill karna hai)
 
-            // Jab tak dono arrays me elements hain
-            while (i >= 0 && j >= 0) {
-                if (nums1[i] > nums2[j]) {
-                    nums1[k] = nums1[i];
-                    i--;
-                } else {
-                    nums1[k] = nums2[j];
-                    j--;
-                }
-                k--;
-            }
-
-            // Agar nums2 ke elements bache hain
-            while (j >= 0) {
-                nums1[k] = nums2[j];
-                j--;
-                k--;
-            }
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
         }
-    
+
+        sort(nums1.begin(),nums1.end());
+        
+        
+    }
 };
