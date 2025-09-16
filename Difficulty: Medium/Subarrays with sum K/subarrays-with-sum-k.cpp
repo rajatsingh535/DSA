337e693d6d880b458f7c2e94603a81acae5cb850
RAJATSINGH <rajat.singh20242@lpu.in>
2025-09-16 17:16:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -1,23 +1,26 @@
 class Solution {
   public:
     int cntSubarrays(vector<int> &arr, int k) {
-       int n = arr.size();
-       unordered_map<int , int> map;
-       int result = 0;
-       int cumsum = 0;
-       map.insert({0,1});
-       for(int i = 0; i < n  ;  i++){
-           cumsum += arr[i];
-           
-           if(map.find(cumsum-k)!=map.end()){ 
-               result += map[cumsum-k];
-           }
-           
-           map[cumsum]++;
-           
-       }
-       
-       return result;
+        int n = arr.size();
+        
+        unordered_map<int , int> mp;
+        
+        int sum = 0 , cnt = 0;
+        
+        mp.insert({0,1});
+        
+        
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            if(mp.find(sum-k)!=mp.end()){
+                cnt += mp[sum-k];
+            }
+            
+            mp[sum]++;
+        }
+        
+        
+        return cnt;
         
     }
 };
