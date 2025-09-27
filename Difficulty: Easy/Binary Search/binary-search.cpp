fac5022efd9ece2a4af924ea15d81ce170c693d2
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-09-27 10:01:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.cpp
+++ Difficulty: Easy/Binary Search/binary-search.cpp
@@ -1,27 +1,29 @@
 class Solution {
   public:
     int binarysearch(vector<int> &arr, int k) {
-   int low = 0;
-    int high = arr.size() - 1;
-    int result = -1; // To store the potential answer
-
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
-
-      if (arr[mid] == k) {
-        // Found an occurrence. Store it and try to find an earlier one.
-        result = mid;
-        high = mid - 1; // Continue searching in the left half
-      } else if (arr[mid] < k) {
-        // Search in the right half
-        low = mid + 1;
-      } else {
-        // Search in the left half
-        high = mid - 1;
-      }
+        int low = 0;
+        int high = arr.size() - 1;
+        int result = -1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid]==k) {
+                result = mid;
+                high = mid - 1;
+                
+            }
+            
+            else if (k > arr[mid]) {
+                low = mid + 1;
+                
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        
+        return result;
+        
     }
 
-    return result;
-    }
 };
-
