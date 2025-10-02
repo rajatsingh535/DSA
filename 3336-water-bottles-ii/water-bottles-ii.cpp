0b972da1b78d57c52f4566f35a505b13da5900b7
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-10-02 11:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (77.56%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = 0;
+        while(numBottles >= numExchange){
+            ans += numExchange;
+            numBottles -= numExchange;
+            numBottles++;
+            numExchange++;
+        }
+
+        ans += numBottles;
+        return ans;
+        
+    }
+};
