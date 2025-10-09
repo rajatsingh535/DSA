6d504ea95aa768daa5786b46730c96e4adb91106
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-10-09 23:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (28.66%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
+
         int n = nums.size();
-        int check = n/2;
+        int majority = nums[0] , votes = 1;
 
-        unordered_map<int , int> map;
-        for(int i = 0; i < n; i++){
-            map[nums[i]]++;
+        for(int i = 1; i < n; i++){
+            if(votes==0){
+                votes++;
+                majority = nums[i];
+            }
+            else if(majority==nums[i]){
+                votes++;
+            }
+            else{
+                votes--;
+            }
         }
-
-        for(auto i : map){
-            int f = i.second;
-            int n = i.first;
-            if(f > check) return n;
-        }
-
-        return -1;
-
+        
+        return majority;
+    
         
     }
 };
