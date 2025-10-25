bca4deb3463f4b16f1f27cc76d264a3c11656125
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-10-25 10:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp(nums1.begin(), nums1.begin()+m);
 
-        for(int i = 0; i < n; i++){
-            nums1[m+i] = nums2[i];
+        int i = 0 , j = 0 , k = 0;
+
+        while(i < m && j < n){
+            if(temp[i] < nums2[j]){
+                nums1[k] = temp[i];
+                k++; i++;
+            }
+            else{
+                nums1[k] = nums2[j];
+                k++; j++;
+            }
         }
 
-        sort(nums1.begin(),nums1.end());
-        
+
+        while(i < m) nums1[k++] = temp[i++];
+        while(j < n) nums1[k++] = nums2[j++];
         
     }
 };
