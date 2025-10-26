e79323ca54d542b1ca8dc422cdfb86b417e3ae49
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-10-26 18:49:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Function that returns double the value of n
+int returnValueFunction(int n) {
+    return 2 * n;   // return double the value
+}
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     cout << returnValueFunction(n);
+//     return 0;
+// }
