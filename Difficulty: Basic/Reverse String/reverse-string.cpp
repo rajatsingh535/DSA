eaf81687272499836a34c6ed233afc3e42fbc496
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-10-31 19:55:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse String/reverse-string.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+// the function should return the reversed string
+string reverseString(string &s) {
+    // Method 1: Using built-in reverse function
+    reverse(s.begin(), s.end());
+    return s;
+}
