c1508fa53a287c28685833b1447442c127470169
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-11-10 22:41:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int arraySum(int arr[], int size) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+};
