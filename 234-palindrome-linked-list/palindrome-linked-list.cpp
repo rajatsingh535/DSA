3cdff5627d75c5c4052b453e9b6c6230ac9d304c
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-11-20 23:31:20 +0530

    Time: 7 ms (29.95%) | Memory: 132.1 MB (15.12%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         vector<int> arr;
-
         while (head != nullptr) {
             arr.push_back(head->val);
             head = head->next;
         }
+
+      
 
         int left = 0;
         int right = arr.size() - 1;
