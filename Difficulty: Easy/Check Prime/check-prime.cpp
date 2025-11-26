c80b9d67eba7ff6412893adf3ddb3b653eda183f
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-11-26 23:04:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.cpp
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -1,10 +1,30 @@
-bool prime(int n) {
-    if (n < 2) return false;  // 0 and 1 are not prime
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
-            return false;
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 1) {
+        cout << "False";
+        return 0;
     }
 
-    return true;
+    bool isPrime = true;
+
+    // Check divisors from 2 to sqrt(n)
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) 
+        cout << "True";
+    else 
+        cout << "False";
+
+    return 0;
 }
