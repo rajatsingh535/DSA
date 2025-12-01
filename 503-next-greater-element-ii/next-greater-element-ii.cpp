105a30037ace55399004fdcd2f554817a55276ea
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-12-01 23:45:54 +0530

    Time: 1 ms (83.80%) | Memory: 28.4 MB (58.61%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return {};
-        vector<int> res(n, -1);
-        vector<int> st; // acts as s
+        vector<int> ans(n);
+        stack<int> s;
+        for(int i = 2 * n-1; i>=0; i--){
+            int idx = i % n;
 
-        // iterate from right to left across a logical doubled array
-        for (int i = 2 * n - 1; i >= 0; --i) {
-            int cur = nums[i % n];
-            while (!st.empty() && st.back() <= cur) st.pop_back();
-            if (i < n) res[i] = st.empty() ? -1 : st.back();
-            st.push_back(cur);
+
+            while(!s.empty() && s.top() <= nums[idx]){
+                s.pop();
+            }
+
+            ans[idx] = s.empty() ? -1:s.top();
+
+            s.push(nums[idx]);
         }
-        return res;
+
+        return ans;
+      
     }
 };
