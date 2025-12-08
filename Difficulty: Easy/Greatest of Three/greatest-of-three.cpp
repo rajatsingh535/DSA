8db29617c9bc97c13e42d633af455c35145c2852
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-12-08 22:34:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Greatest of Three/greatest-of-three.cpp
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;   // taking input
-
-    // find greatest
-    int greatest = a;
-
-    if(b > greatest)
-        greatest = b;
-    if(c > greatest)
-        greatest = c;
-
-    cout << greatest;
-
-    return 0;
+int find_greatest_number(int a, int b, int c) {
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
 }
