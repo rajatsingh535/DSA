ca8d2606d2626420c67ddcd3f9f65cffecce6faa
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-12-17 23:45:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Alphabets/print-alphabets.cpp
@@ -0,0 +1,7 @@
+void alphabets(char c1, char c2) {
+    // Start from c1 and loop until c2 (inclusive)
+    for (char i = c1; i <= c2; i++) {
+        // Print the character followed by a space
+        cout << i << " ";
+    }
+}
