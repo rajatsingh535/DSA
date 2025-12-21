f29b1a3bf8c00e333bc00824054a137e8748e7d3
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-12-21 22:58:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        vector<int> ans;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int idx = abs(arr[i]) - 1;
+
+            if (arr[idx] < 0) {
+                ans.push_back(abs(arr[i]));
+            } else {
+                arr[idx] = -arr[idx];
+            }
+        }
+
+        return ans;
+    }
+};
