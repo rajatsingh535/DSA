9a2dffb12f7eb47130eef2173cabd55a172aec8e
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2025-12-26 23:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    bool findTriplets(vector<int> &arr) {
+        int n = arr.size();
+        if (n < 3) return false;
+
+        sort(arr.begin(), arr.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0)
+                    return true;
+                else if (sum < 0)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return false;
+    }
+};
