6cff00750a8d317f2dea636cc7c842cd8369a9e9
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-01-27 20:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (83.66%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int near = 0, far = 0, jumps = 0;
 
-        while (far < nums.size() - 1) {
-            int farthest = 0;
-            for (int i = near; i <= far; i++) {
-                farthest = max(farthest, i + nums[i]);
+        int n = nums.size();
+
+        int total = 0;
+
+        int cov = 0 , des = n - 1 , lastjump = 0;
+
+        if( n == 1) return 0;
+
+        for(int i = 0; i < n; i++){
+            cov = max(cov , nums[i] + i);
+
+            if(i == lastjump){
+                lastjump = cov;
+                total++;
+
+                if(cov >= des){
+                    return total;
+                }
             }
-            near = far + 1;
-            far = farthest;
-            jumps++;
         }
 
-        return jumps;        
+        return total;
+        
     }
 };
