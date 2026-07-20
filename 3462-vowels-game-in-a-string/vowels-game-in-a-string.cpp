baef042773bb5e2fc4b518242a55068916812647
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-07-20 11:13:31 +0530

    Time: 2 ms (84.88%) | Memory: 19 MB (55.21%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    bool isVowel(char c){
-        return c == 'a'||c == 'e'||c =='i'||c=='o'||c=='u';
-    }
     bool doesAliceWin(string s) {
-        int cnt = 0;
-
-        for(char c : s){
-            if(isVowel(c)) cnt ++;
+        for (char c : s) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true; 
+            }
         }
-        return cnt > 0;
-        
+        return false; 
     }
 };
-
-
