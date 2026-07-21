5d60f59770f6b7b8944937ae2996740bc8b0f666
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-07-21 12:00:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,10 +1,17 @@
 class Solution {
-public:
-    int kthSmallest(vector<int> &arr, int k) {
-        // Sort the array in ascending order
-        sort(arr.begin(), arr.end());
-        
-        // Return the kth smallest element (1-based index)
-        return arr[k - 1];
-    }
+	public:
+	int kthSmallest(vector<int> &arr, int k) {
+		
+		priority_queue<int> maxHeap;
+		
+		for (int num : arr) {
+			maxHeap.push(num);
+			
+			if (maxHeap.size() > k) {
+				maxHeap.pop();
+			}
+		}
+		
+		return maxHeap.top();
+	}
 };
