b3c644b544fee9f04c2477ffac71327b7839068e
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-07-22 09:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (13.19%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(std::vector<int>& stones) {
+        
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+
+     
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.top(); 
+            maxHeap.pop();
+            int x = maxHeap.top();
+            maxHeap.pop();
+
+            if (x != y) {
+                maxHeap.push(y - x);
+            }
+        }
+
+
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
