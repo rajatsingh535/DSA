092958abe3cd23bf576fa5d89d6723e81c0af359
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-07-28 23:39:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -1,24 +1,22 @@
 class Solution {
   public:
     int convertFive(int n) {
-        
-        if(n == 0) return 5;
-    
-    
-        long long ans = 0;
-        int place = 1;
-        while(n != 0){
-            
+       if(n == 0) return 5;
+       int mul = 1;
+       int ans = 0;
+       while(n != 0)
+        {
             int digit = n % 10;
+            if(digit == 0){
+                digit = 5;
+            }
             
-            if(digit == 0) digit = 5;
-            ans = ans +  digit * place;
-            place = place *  10;
+            ans = ans + digit * mul;
+            mul *= 10;
             n = n / 10;
+            
         }
         
-        
         return ans;
-        
     }
 };
