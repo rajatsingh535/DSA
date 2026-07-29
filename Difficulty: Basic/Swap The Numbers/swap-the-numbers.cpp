33f454c31952af8129dcd4eca20ee5cb42dc7e5c
Rajat Singh | GSSoC '25 Contributor <rajat.singh20242@lpu.in>
2026-07-29 10:44:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -1,5 +1,14 @@
-void swapNumbers(int &a, int &b) {
-    a ^= b;
-    b ^= a;
-    a ^= b;
+void swap(int &a, int &b) {
+	
+	a = a ^ b;
+	
+	b = b ^ a;
+	
+	a = a ^ b;
+	
+	// step 1 a ^ b
+	// step 2   b ^ a ^ b = a hence b swap;
+	// step 3    a = a * b = a ^ b ^ a = b  hence a swap
+
+	
 }
